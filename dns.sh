933af55c2806019f8e9044b30b9fe9811f809458
Julak Bantur <137990782+bagusid93@users.noreply.github.com>
2023-09-23 13:20:08 +0800

    Create dns.sh

---
+++ dns.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+tyblue='\e[1;36m'
+NC='\e[0m'
+purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
+tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+#########################
+
+clear
+yellow "Add Domain for Ssh/vmess/vless/trojan dll"
+echo " "
+echo -e "$green      Please select a domain type below               $NC"
+echo  ""
+tyblue "    1 : Enter your Subdomain & Nsdomain"
+tyblue "    2 : Use a random Subdomain & Nsdomain"
+echo ""
+read -p "   Please select numbers 1-2 or Any Button(Random) : " host
+echo ""
+if [[ $host == "1" ]]; then
+read -rp "Enter Your Domain / masukan domain : " pp
+read -rp "Input ur ns-domain : " -e nsdomen
+echo "IP=$pp" > /var/lib/ipvps.conf
+echo "$pp" > /root/domain
+echo "$pp" > /root/scdomain
+echo "$pp" > /etc/xray/domain
+echo "$pp" > /etc/v2ray/domain
+echo "$pp" > /etc/xray/scdomain
+echo "$nsdomen" > /etc/xray/dns
+echo "$nsdomen" > /root/nsdomain
+echo ""
+elif [[ $host == "2" ]]; then
+#install Domain
+wget https://raw.githubusercontent.com/bagusid93/sc3/main/ssh/cf.sh && chmod +x cf.sh && ./cf.sh
+rm -f /root/cf.sh
+clear
+else
+echo -e "Random Subdomain/Domain is used"
+wget https://raw.githubusercontent.com/bagusid93/sc3/main/ssh/cf.sh && chmod +x cf.sh && ./cf.sh
+rm -f /root/cf.sh
+sleep 1
+clear
+#install NameServer
+wget https://raw.githubusercontent.com/bagusid93/sc3/main/slowdns/ns-domain.sh && chmod +x ns-domain.sh && ./ns-domain.sh
+rm -f /root/ns-domain.sh
+clear
+else
+echo -e "Random Nsdomain/Nsdomain is used"
+wget https://raw.githubusercontent.com/bagusid93/sc3/main/slowdns/ns-domain.sh && chmod +x ns-domain.sh && ./ns-domain.sh
+rm -f /root/ns-domain.sh
+fi
